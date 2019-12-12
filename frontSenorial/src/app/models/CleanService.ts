01d77f827f2987a636eb563c6ca8ffa3e23213f8
daniel hizo un comentario <daniellopezj0327@gmail.com>
2019-12-12 15:08:50 -0500

    agregar modelo de servicio de limpieza

---
+++ frontSenorial/src/app/models/CleanService.ts
@@ -0,0 +1,15 @@
+export class CleanService {
+    public nombre: String;
+    public apellido: String;
+    public cedula: String;
+    public celular: String;
+    public direccion: String;
+    public Correo: String;
+    public id_user: number;
+    public estado: String;
+    public valor: number;
+    public fecha_servicio: String;
+    public horario: String;
+    public jornada: String;
+    public NColaboradores: number;
+}
