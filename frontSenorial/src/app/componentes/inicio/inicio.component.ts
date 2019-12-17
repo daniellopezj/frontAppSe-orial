ded12e9ef10bbef2cabf288643b25a6bc4b18d5f
daniel hizo un comentario <daniellopezj0327@gmail.com>
2019-12-17 14:01:06 -0500

    agregar select a los servicios colocados

--- frontSenorial/src/app/componentes/inicio/inicio.component.ts
+++ frontSenorial/src/app/componentes/inicio/inicio.component.ts
@@ -1,3 +1,4 @@
+import { ServicePerson } from './../../Services/ServicePerson';
 import { ServiceCleanService } from './../../Services/service-clean.service';
 import { Component, OnInit } from '@angular/core';
 import { WebsocketService } from 'src/app/Services/websocket.service';
@@ -11,11 +12,16 @@ import { FormControl } from '@angular/forms';
 export class InicioComponent implements OnInit {
   showinfo: boolean;
   listInfo: any;
-  toppings = new FormControl();
+  listColaboradores: any;
+  mascolaboradores = new FormControl();
+  multipleCol: String;
+  selected = '';
   toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
-  constructor(private websocket: WebsocketService, private serviceClean:ServiceCleanService) {
+  mySelections: string[];
+
+  constructor(private websocket: WebsocketService, private servicePerson: ServicePerson, private serviceClean: ServiceCleanService) {
     this.loadServicePending();
-   }
+  }
 
   ngOnInit() {
     this.websocket.listen('test-event').subscribe((data) => {
@@ -28,13 +34,25 @@ export class InicioComponent implements OnInit {
     this.showinfo = false;
     this.serviceClean.requestServicePending().subscribe(res => {
       if (res.responseCode == 200) {
-        this.listInfo = res.object;
-        this.showinfo = true;
+        this.servicePerson.requestPerson().subscribe(res => {
+          if (res.responseCode == 200) {
+            this.listColaboradores = res.object;
+            this.showinfo = true;
+          }
+        });
+            this.listInfo = res.object;
       } else {
         console.log("ocurrio un fallo")
       }
     });
   }
 
+  changed(nColaboradores) {
+    if (this.mascolaboradores.value.length <= nColaboradores) {
+      this.mySelections = this.mascolaboradores.value;
+    } else {
+      this.mascolaboradores.setValue(this.mySelections);
+    }
+  }
 
 }
